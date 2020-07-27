5d78632d18f7d2eec1f7f8b9196444edb361f9df
William Reiske <wreiske@mieweb.com>
2020-07-27 09:47:32 -0400

    Update run-builder.sh

--- run-builder.sh
+++ run-builder.sh
@@ -7,7 +7,20 @@ TARGET="mongodb-linux-x86_64-${MONGODB_VERSION}"
 BIN="$TARGET/bin"
 
 [ ! -d $SRC ] && curl "https://fastdl.mongodb.org/src/$SRC.tar.gz" | tar -xz
-docker run --rm -t -v $(pwd)/$SRC:/mongodb mongodb-builder
 mkdir -p $BIN
 mv "$SRC/mongo" "$SRC/mongod" $BIN
 tar -czf "$TARGET.tgz" $TARGET
+
+set -e
+cd /mongodb
+pip3 install -r etc/pip/compile-requirements.txt
+
+./buildscripts/scons.py \
+  mongod mongo \
+  --ssl=off \
+  --enable-free-mon=off \
+  LINKFLAGS='-static-libstdc++' \
+  CC=gcc \
+  CXX=g++
+
+strip mongo mongod
